6df7a3c64b996ba7e8fad53b123c5620e3ae372f
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-09-02 17:44:23 +0100

    added new custom widget in the code

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
+      home: Scaffold(
+        body: 
       ),
-      home: HomePage(),
     );
+  }
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 80),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello Worrld',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        );
   }
 }
