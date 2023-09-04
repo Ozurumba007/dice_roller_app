28be7fe3a7218ea39430baeee9bf1a6b45299a7f
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-09-04 09:51:09 +0100

    added a sizedebox to the dice roller file \ finished application

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:picture_changer/dice_roller.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 // ignore: must_be_immutable
 class GradientContainer extends StatelessWidget {
-  GradientContainer({
+  const GradientContainer({
     super.key,
     required this.colors,
   });
   final List<Color> colors;
-
-  var activeDiceImage = 'assets/images/dice-2.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-    print('Changing Image....');
-  }
 
   @override
   Widget build(context) {
@@ -28,24 +22,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 200,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                  padding: EdgeInsets.only(top: 20),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(fontSize: 28)),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
